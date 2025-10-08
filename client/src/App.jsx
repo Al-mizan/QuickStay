24a11b72fddb2a1a9b6db443e70bea889c1ebc51
Al-mizan <mdalmizanakon@gmail.com>
2025-10-08 15:33:41 +0600

    again create MyBookings page, this page is accessible by clicking profile picture

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import AllRooms from "./pages/AllRooms";
 import RoomDetails from "./pages/RoomDetails";
+import MyBookings from "./pages/MyBookings";
 
 export default function App() {
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -16,6 +17,7 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/rooms" element={<AllRooms />} />
           <Route path="/rooms/:id" element={<RoomDetails />} />
+          <Route path="/my-bookings" element={<MyBookings />} />
         </Routes>
       </main>
       <Footer />
